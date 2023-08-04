81ed67968962395fc7ab49e25a888bf66e7ed547
Ubuntu <azureuser@Ubuntu.2eo5tgtcld0engwzfkeaqmc1fe.bx.internal.cloudapp.net>
2023-08-04 04:49:06 +0000

    Changed Firstjava.java

--- Firstjava.java
+++ Firstjava.java
@@ -5,7 +5,7 @@ public class Firstjava
 
         public static void main(String[] args) {
 
-                System.out.println("Hello welcome to jenkins");
+                System.out.println("Hello welcome to jenkins! Demonstrating Freestyle job.");
 
         }
 
